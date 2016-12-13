df877f318aeea01d50b3ec6a615bd18dc59a1645
yinyayun <yinyayun1314@gmail.com>
2016-12-13 18:00:58 +0800

    模块导入测试

---
+++ advance-demo/Calc.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+
+def add(x,y):
+    return x+y
+
+def multiply(x,y):
+    return x*y
---
+++ advance-demo/import-demo.py
@@ -0,0 +1,4 @@
+#coding:utf-8
+#也可以直接import Calc,但是使用add时需要域限定
+from Calc import add,multiply
+print multiply(1,2)
